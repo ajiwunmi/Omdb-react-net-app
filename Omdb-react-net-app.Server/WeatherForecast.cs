96b692de1d9341af57cff44eef5797bd3f668379
Ajiwunmi <ajiwunmi@gmail.com>
2024-09-24 11:06:53 +0100

    Add project files.

---
+++ Omdb-react-net-app.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Omdb_react_net_app.Server
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
